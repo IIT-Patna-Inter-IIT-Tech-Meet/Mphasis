642d507a34e683e54956e2ea6cdf77b04785ca29
Rupak Biswas <rupak_2001cs57@iitp.ac.in>
2023-11-22 20:11:00 +0530

    [+] Models added

--- app/flight/models.py
+++ app/flight/models.py
@@ -38,7 +38,46 @@ class Airport(models.Model):
     def __str__(self):
         return f"[{self.iata_code}-{self.ident}]"
 
+class Aircraft(models.Model):
+    id = models.CharField(max_length=255, primary_key=True, unique=True, null=False)
+    model = models.CharField(max_length=25, null=False)
+    name = models.CharField(max_length=255, null=False)
+    registration = models.CharField(max_length=255, null=False)
+    owner_code = models.CharField(max_length=255, null=False)
+    owner_name = models.CharField(max_length=255, null=False)
+    cabin_type = models.CharField(max_length=255, null=False, blank=True)
 
+    def __str__(self):
+        return f"[{self.id_num}-{self.id_callsign}]"
+
+class ClassType(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    type_name = models.CharField(max_length=255, null=False)
+
+class CabinType(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    type_name = models.CharField(max_length=255, null=False)
+
+class SeatDistribution(models.Model):
+    aircraft_id = models.ForeignKey(Aircraft, on_delete=models.CASCADE, null=False)
+    class_type = models.ForeignKey(ClassType, on_delete=models.CASCADE, null=False)
+    cabin_type = models.ForeignKey(CabinType, on_delete=models.CASCADE, null=False)
+    seat_count = models.IntegerField(null=False, default=0)
+
+
+class Flight(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    flight_number = models.CharField(max_length=255, null=False)
+    aircraft_id = models.ForeignKey(Aircraft, on_delete=models.CASCADE, null=False)
+    departure_airport_id = models.ForeignKey(Airport, on_delete=models.CASCADE, null=False, related_name='departure_airport')
+    arrival_airport_id = models.ForeignKey(Airport, on_delete=models.CASCADE, null=False, related_name='arrival_airport')
+    departure_time = models.DateTimeField(null=False)
+    arrival_time = models.DateTimeField(null=False)
+    flight_time = models.IntegerField(null=False)
+    status = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return f"[{self.flight_number}-{self.aircraft_id}-{self.departure_airport_id}-{self.arrival_airport_id}]"
 
     
 
