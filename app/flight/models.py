0f8946cc98872354ed3f3482909d7ae553ccf93b
Sanskriti Singh <sanskriti180903@gmail.com>
2023-11-24 02:15:24 +0530

    [.] PNR model

--- app/flight/models.py
+++ app/flight/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from enum import Enum
+from django.contrib.postgres.fields import ArrayField
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Airport(models.Model):
@@ -51,18 +53,31 @@ class Aircraft(models.Model):
         return f"[{self.id_num}-{self.id_callsign}]"
 
 class ClassType(models.Model):
+    types = [
+        ('A', 'First Class'),
+        ('C', 'Business Class'),
+        ('K', 'Economy Class')
+    ]
+    
     id = models.AutoField(primary_key=True, auto_created=True)
-    type_name = models.CharField(max_length=255, null=False)
+    type_name = models.CharField(max_length=255, null=False, choices=types)
 
 class CabinType(models.Model):
+    types = [
+        ('F', 'First Cabin'),
+        ('J', 'Second Cabin'),
+        ('Y', 'Third Cabin')
+    ]
+    
     id = models.AutoField(primary_key=True, auto_created=True)
-    type_name = models.CharField(max_length=255, null=False)
+    type_name = models.CharField(max_length=255, null=False, choices=types)
 
 class SeatDistribution(models.Model):
     aircraft_id = models.ForeignKey(Aircraft, on_delete=models.CASCADE, null=False)
     class_type = models.ForeignKey(ClassType, on_delete=models.CASCADE, null=False)
     cabin_type = models.ForeignKey(CabinType, on_delete=models.CASCADE, null=False)
     seat_count = models.IntegerField(null=False, default=0)
+    seat_avail = models.IntegerField(null=False, default=0)
 
 
 class Flight(models.Model):
@@ -78,7 +93,80 @@ class Flight(models.Model):
 
     def __str__(self):
         return f"[{self.flight_number}-{self.aircraft_id}-{self.departure_airport_id}-{self.arrival_airport_id}]"
+    
+class Passenger(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    phone_no = PhoneNumberField(blank=False, null=False) 
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    
+    def __str__(self):
+        return f"[{self.first_name}-{self.last_name}-{self.phone_no}-{self.email}]"
+    
+class PassengerSeat(models.Model):
+    ssr_types = [
+        ('INFT' , 'Infant'),
+        ('WCHR' , 'Wheelchair, can walk'),
+        ('WCHS' , 'Wheelchair, can\'t climb stairs'),
+        ('WCHC' , 'Complete immobile'),
+        ('LANG' , 'Language restrictions'),
+        ('CHLD' , 'Child'),
+        ('MAAS' , 'Meet and assist - many reasons'),
+        ('UNMR' , 'Unaccompanied minor'),
+        ('BLND' , 'Blind'),
+        ('DEAF' , 'Deaf'),
+        ('EXST' , 'Large person taking up two seats'),
+        ('MEAL' , 'Meal request'),
+        ('NSST' , 'seat information'),
+        ('NRPS' , 'No seat request')
+    ]
+    
+    id = models.AutoField(primary_key=True, auto_created=True)
+    passenger = models.ForeignKey('passenger.Passenger', on_delete=models.CASCADE, null=False)
+    pnr = models.ForeignKey('PNR', on_delete=models.CASCADE, null=False)
+    seat_number = models.CharField(max_length=255, null=False)
+    seat_class = models.ForeignKey(ClassType, on_delete=models.CASCADE, null=False)
+    seat_cabin = models.ForeignKey(CabinType, on_delete=models.CASCADE, null=False)
+    seat_price = models.FloatField(null=False)
+    seat_tax = models.FloatField(null=False)
+    seat_total = models.FloatField(null=False)
+    ssr = models.CharField(max_length=255, null=True, choices=ssr_types)
+    
+    def save(self, *args, **kwargs):
+        # Decrease seat_avail by one when a new PassengerSeat is created
+        self.seat_distribution.seat_avail -= 1
+        self.seat_distribution.save()
+        
+        # Calculate seat_total
+        self.calculate_seat_total()
+
+        # Call the original save method
+        super(PassengerSeat, self).save(*args, **kwargs)
 
     
+    def calculate_seat_total(self):
+        # Calculate the total price of the seat
+        self.seat_total = self.seat_price + self.seat_tax
+        self.save()
+
+    def __str__(self):
+        return f"[{self.flight}-{self.passenger}-{self.seat_number}-{self.seat_class}-{self.seat_cabin}-{self.ssr}]"
+
+class PNR(models.Model):
+    id = models.AutoField(primary_key=True, auto_created=True)
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, null=False)
+    seats = models.ManyToManyField(PassengerSeat)
+    timestamp = models.DateTimeField(null=False)
+    total_price = models.FloatField(null=False, default=0)
+    seat_currency = models.CharField(max_length=255, null=False)
+
+    def calculate_total_price(self):
+        # Sum up the total prices of all PassengerSeat instances
+        self.total_price = sum(seat.seat_total for seat in self.seats.all())
+        self.save()
+
+    def __str__(self):
+        return f"[{self.id}-{self.flight}-{self.total_price}-{self.seat_currency}]"
 
 
