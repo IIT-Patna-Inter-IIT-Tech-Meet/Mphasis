7937d9fc5a956718b166facac49adaed0733b010
Rupak Biswas <rupak_2001cs57@iitp.ac.in>
2023-11-24 10:27:03 +0530

    [+] Dataingested

--- app/flight/models.py
+++ app/flight/models.py
@@ -39,16 +39,16 @@ class Airport(models.Model):
         return f"[{self.iata_code}-{self.ident}]"
 
 class Aircraft(models.Model):
-    id = models.CharField(max_length=255, primary_key=True, unique=True, null=False)
+    id = models.AutoField(primary_key=True, auto_created=True, unique=True)
     model = models.CharField(max_length=25, null=False)
-    name = models.CharField(max_length=255, null=False)
+    name = models.CharField(max_length=255, null=True, blank=True)
     registration = models.CharField(max_length=255, null=False)
     owner_code = models.CharField(max_length=255, null=False)
     owner_name = models.CharField(max_length=255, null=False)
     cabin_type = models.CharField(max_length=255, null=False, blank=True)
 
     def __str__(self):
-        return f"[{self.id_num}-{self.id_callsign}]"
+        return f"[{self.registration}-{self.owner_code}]"
 
 class ClassType(models.Model):
     id = models.AutoField(primary_key=True, auto_created=True)
@@ -66,7 +66,7 @@ class SeatDistribution(models.Model):
 
 
 class Flight(models.Model):
-    id = models.AutoField(primary_key=True, auto_created=True)
+    id = models. CharField(max_length=255, primary_key=True, unique=True, null=False)
     flight_number = models.CharField(max_length=255, null=False)
     aircraft_id = models.ForeignKey(Aircraft, on_delete=models.CASCADE, null=False)
     departure_airport_id = models.ForeignKey(Airport, on_delete=models.CASCADE, null=False, related_name='departure_airport')
